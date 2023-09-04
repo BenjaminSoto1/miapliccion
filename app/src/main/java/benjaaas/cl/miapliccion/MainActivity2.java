09f331c1da7f9037528126a6d6c93237afb2ebf5
Alumnos <benjamin.soto1@virginiogomez.cl>
2023-09-04 11:47:39 -0300

    Initial commit

---
+++ app/src/main/java/benjaaas/cl/miapliccion/MainActivity2.java
@@ -0,0 +1,14 @@
+package benjaaas.cl.miapliccion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
