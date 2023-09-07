14538277d8defd3b7098b0418f0316e4ce09a303
PC <benjamin.soto1@virginiogomez.cl>
2023-09-06 22:39:58 -0300

    dsada

--- app/src/main/java/benjaaas/cl/miapliccion/MainActivity.java
+++ app/src/main/java/benjaaas/cl/miapliccion/MainActivity.java
@@ -1,13 +1,40 @@
 package benjaaas.cl.miapliccion;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imagebutton2;
+    Button button1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this,MainActivity2.class);
+        imagebutton2=(ImageButton) findViewById(R.id.imageButton2);
+        Intent intent1 = new Intent(this,MainActivity3.class);
+        button1=(Button) findViewById(R.id.button3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent1);
+            }
+        });
+
+        imagebutton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/benjaaas/cl/miapliccion/MainActivity3.java
+++ app/src/main/java/benjaaas/cl/miapliccion/MainActivity3.java
@@ -6,22 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
+
 
     Button button1;
+    EditText respuesta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main3);
         Intent intent = new Intent(this,MainActivity.class);
-        button1=(Button) findViewById(R.id.button);
-
+        button1=(Button) findViewById(R.id.button2);
+        respuesta=(EditText) findViewById(R.id.eTrespuesta);
+        intent.putExtra("respuesta",respuesta.getText().toString());
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent);
             }
         });
+
     }
 }
